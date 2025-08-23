1888b6b76a70517541455dfceac1fded36f2c190
Stephen Higgins <stephen.higgins1995@gmail.com>
2025-08-23 19:36:29 +0100

    Refactor profile page and replace ParallaxFlatList
    Replaced ParallaxFlatList with FlatList for improved simplicity and maintainability. Updated user profile header layout to include new stats section and adjusted styling. Removed RageOdometer and commented out related code temporarily.

--- src/app/(app)/(tabs)/(profile)/profile.tsx
+++ src/app/(app)/(tabs)/(profile)/profile.tsx
@@ -1,7 +1,6 @@
 import { useSupabaseQuery } from '@/src/api/hooks/common/use-supabase-query';
 import { getPosts } from '@/src/api/methods/posts/get-posts';
 import { Page } from '@/src/components/page';
-import { RageOdometer } from '@/src/components/pages/profile/ragometer';
 import { Post } from '@/src/components/pages/tabs/home/feed/posts/post';
 import { useCurrentUser } from '@/src/context/auth-context';
 import { useLocationContext } from '@/src/context/location-context';
@@ -10,9 +9,9 @@ import { UserDto } from '@/src/types/user.types';
 import { Body, BodyType } from '@ui/body';
 import { Button } from '@ui/button';
 import { Headline, HeadlineType } from '@ui/healine';
-import { ParallaxFlatList } from '@ui/parallax-flat-list';
 import { Image } from 'expo-image';
 import React, { type FC } from 'react';
+import { FlatList } from 'react-native';
 import { View, XStack, YStack } from 'tamagui';
 
 const UserProfileHeader = ({ user }: { user: UserDto }) => {
@@ -20,49 +19,61 @@ const UserProfileHeader = ({ user }: { user: UserDto }) => {
 		'|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
 	return (
-		<View alignItems="center" px="$md" gap="$sm">
-			<View
-				w={100}
-				h={100}
-				borderRadius={50}
-				jc="center"
-				alignItems="center"
-				bg="$primary40"
-			>
-				<View w={90} h={90} borderRadius={50} overflow="hidden">
-					<Image
-						style={{ flex: 1 }}
-						source={
-							user.profilePhoto
-								? { uri: user.profilePhoto }
-								: require('@/assets/images/icon-dark.png')
-						}
-						cachePolicy="memory-disk"
-						placeholder={{ blurhash }}
-						contentFit="cover"
-						transition={1000}
-					/>
+		<View px="$md" gap="$sm" pt="$md">
+			<XStack alignItems="center" gap="$lg">
+				<View
+					w={100}
+					h={100}
+					borderRadius={50}
+					jc="center"
+					alignItems="center"
+					bg="$primary40"
+				>
+					<View w={90} h={90} borderRadius={50} overflow="hidden">
+						<Image
+							style={{ flex: 1 }}
+							source={
+								user.profilePhoto
+									? { uri: user.profilePhoto }
+									: require('@/assets/images/icon-dark.png')
+							}
+							cachePolicy="memory-disk"
+							placeholder={{ blurhash }}
+							contentFit="cover"
+							transition={1000}
+						/>
+					</View>
 				</View>
-			</View>
+				<XStack f={1} alignItems="center" gap="$md" jc="space-between">
+					<YStack alignItems="center">
+						<Body variant={BodyType.bold}>1.2k</Body>
+						<Body c="$textMuted" variant={BodyType.small}>
+							Rants
+						</Body>
+					</YStack>
+					<YStack alignItems="center">
+						<Body variant={BodyType.bold}>400k</Body>
+						<Body c="$textMuted" variant={BodyType.small}>
+							Replies
+						</Body>
+					</YStack>
+					<YStack alignItems="center">
+						<Body variant={BodyType.bold}>1.2M</Body>
+						<Body c="$textMuted" variant={BodyType.small}>
+							Likes
+						</Body>
+					</YStack>
+				</XStack>
+			</XStack>
 			<Headline c="$primary" variant={HeadlineType.h4}>
 				@{user.username}
 			</Headline>
 			<XStack>
 				<Button f={1}>Edit Profile</Button>
 			</XStack>
-			<View mt={25}>
-				<RageOdometer rageScore={100} />
-				<XStack jc="space-evenly" w="100%">
-					<YStack jc="center" alignItems="center" p="$md">
-						<Body variant={BodyType.normal}>{user.followerCount}</Body>
-						<Body variant={BodyType.small}>Followers</Body>
-					</YStack>
-					<YStack jc="center" alignItems="center" p="$md">
-						<Body variant={BodyType.normal}>{user.followingCount}</Body>
-						<Body variant={BodyType.small}>Followers</Body>
-					</YStack>
-				</XStack>
-			</View>
+			{/*<View py="$lg" pb="$xl">*/}
+			{/*	<RageOdometer rageScore={100} />*/}
+			{/*</View>*/}
 		</View>
 	);
 };
@@ -105,21 +116,16 @@ const Profile: FC = () => {
 	}
 
 	return (
-		<Page>
+		<Page isSafeAreaTop>
 			<View f={1} bg="$background">
-				<ParallaxFlatList<PostDto>
+				<FlatList<PostDto>
 					data={posts || []}
 					renderItem={({ item }) => (
 						<View my="$md">
 							<Post post={item as PostDto} />
 						</View>
 					)}
-					headerImage={
-						currentUser.profilePhoto
-							? { uri: currentUser.profilePhoto }
-							: require('@/assets/images/user-header.png')
-					}
-					HeaderOverlay={() => <UserProfileHeader user={currentUser} />}
+					ListHeaderComponent={() => <UserProfileHeader user={currentUser} />}
 				/>
 			</View>
 		</Page>
